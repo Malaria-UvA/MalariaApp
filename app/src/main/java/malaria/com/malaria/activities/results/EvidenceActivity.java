794f164a997f5560a283446eca31e2103f3bddd4
xaviml <xaviml.93@gmail.com>
2019-01-24 13:58:35 +0100

    Changing scroll bar and adding textview to see which field you're looking at.

--- app/src/main/java/malaria/com/malaria/activities/results/EvidenceActivity.java
+++ app/src/main/java/malaria/com/malaria/activities/results/EvidenceActivity.java
@@ -3,7 +3,9 @@ package malaria.com.malaria.activities.results;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -24,6 +26,9 @@ public class EvidenceActivity extends BaseActivity {
     @BindView(R.id.fab)
     FloatingActionButton fab;
 
+    @BindView(R.id.currentItemTV)
+    TextView currentItemTV;
+
     @Inject
     IModelAnalysisService modelAnalysisService;
 
@@ -39,6 +44,25 @@ public class EvidenceActivity extends BaseActivity {
 
         EvidenceAdapter adapter = new EvidenceAdapter(this, images);
 
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) { }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (visibleItemCount == 0) return;
+                int currentFirstPicture = firstVisibleItem + 1;
+                int mostVisibleItem = currentFirstPicture;
+                if (visibleItemCount == 3) {
+                    mostVisibleItem = currentFirstPicture + 1;
+                }
+                if (visibleItemCount == 2 && currentFirstPicture == totalItemCount - 1) {
+                    mostVisibleItem = totalItemCount;
+                }
+                String t = getResources().getString(R.string.seen_pictures, mostVisibleItem, totalItemCount);
+                currentItemTV.setText(t);
+            }
+        });
         listView.setAdapter(adapter);
 
         fab.setOnClickListener(view -> finish());
